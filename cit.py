e1d6e4e86a7d74cf4401f0d738d38985b061a508
Marcuz Corpuz <macoycorpuz06@gmail.com>
2019-09-03 14:32:08 +0900

    adding all memories

--- cit.py
+++ cit.py
@@ -1,5 +1,15 @@
 def solution(totalMemory, foregroundApps, backgroundApps):
     output = []
-    return output
+    appIds, appMemories = [], []
+
+    for fgApp in foregroundApps:
+        for bgApp in backgroundApps:
+            sumOfMemory = fgApp[1] + bgApp[1]
+            appMemories.append(sumOfMemory)
+            appIds.append([fgApp[0], bgApp[0]])
+
+    
+
+    return appIds, appMemories
 
 print(solution(20, [[1,10],[2,15],[3,17]], [[1,3],[2,5]]))
