6f84a1e2d47949361b426012c8911f3a98cbb8d1
Marcuz Corpuz <macoycorpuz06@gmail.com>
2019-09-03 14:50:59 +0900

    final logic

--- cit.py
+++ cit.py
@@ -1,15 +1,25 @@
 def solution(totalMemory, foregroundApps, backgroundApps):
     output = []
-    appIds, appMemories = [], []
+    apps, appMemories = [], []
+    tempMax = 0
 
     for fgApp in foregroundApps:
         for bgApp in backgroundApps:
             sumOfMemory = fgApp[1] + bgApp[1]
             appMemories.append(sumOfMemory)
-            appIds.append([fgApp[0], bgApp[0]])
+            apps.append([fgApp[0], bgApp[0]])
 
-    
+    if min(appMemories) > totalMemory:
+        return []
 
-    return appIds, appMemories
+    for memory in appMemories:
+        if memory > tempMax and memory <= totalMemory:
+            tempMax = memory
+
+    for app, memory in zip(tuple(apps), tuple(appMemories)):
+        if memory == tempMax:
+            output.append(app)
+
+    return output
 
 print(solution(20, [[1,10],[2,15],[3,17]], [[1,3],[2,5]]))
