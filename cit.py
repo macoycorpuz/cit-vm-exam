eb299f829ac02f9dd5ac7ab1fb94a50e1775d0ff
Marcuz Corpuz <macoycorpuz06@gmail.com>
2019-09-03 14:57:00 +0900

    final solution (not optimized)

--- cit.py
+++ cit.py
@@ -8,7 +8,10 @@ def solution(totalMemory, foregroundApps, backgroundApps):
             sumOfMemory = fgApp[1] + bgApp[1]
             appMemories.append(sumOfMemory)
             apps.append([fgApp[0], bgApp[0]])
-
+            
+    if not apps:
+        return []
+    
     if min(appMemories) > totalMemory:
         return []
 
